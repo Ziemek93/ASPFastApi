1f7c161f31223d85b262433103544ed2eb265548
Ziemek93 <maciekxx@gmail.com>
2024-04-02 23:34:10 +0200

    ini

---
+++ ASPFastApi/Entities/Tag.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FastApi.Entity;
+
+    public class Tag
+    {
+    public Tag() 
+    { 
+        Articles = new HashSet<Article>();
+    }
+        [Key]
+        public int TagId { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        [ForeignKey("TagId")]
+        [InverseProperty("Tags")]
+        public virtual ICollection<Article>? Articles { get; set; }
+
+}
+
