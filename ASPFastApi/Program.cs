3b01c408754b6c718550f8a10c919f7227b5d5da
Ziam <mathjeicam@gmail.com>
2024-04-04 00:02:09 +0200

    upd

---
+++ ASPFastApi/Program.cs
@@ -0,0 +1,30 @@
+using FastApi.Context;
+using FastEndpoints;
+using FastEndpoints.Swagger;
+using Microsoft.EntityFrameworkCore;
+
+using System;
+
+public class Program // Now public for easier accessibility
+{
+    static void Main(string[] args)
+    {
+        
+        var builder = WebApplication.CreateBuilder(args);
+        var conntectionString = builder.Configuration.GetConnectionString("BlogConnection");
+        builder.Services.AddScoped<ApplicationContext>();
+
+        builder.Services
+            .AddFastEndpoints()
+            .SwaggerDocument();
+        builder.Services.AddDbContext<FastApi.Context.ApplicationContext>(options => options.UseSqlServer(conntectionString));
+
+        var app = builder.Build();
+        app.UseFastEndpoints(c => {
+            // everything is anonymous for this sample
+            c.Endpoints.Configurator = epd => epd.AllowAnonymous();
+        }).UseSwaggerGen();
+
+        app.Run();
+    }
+}
